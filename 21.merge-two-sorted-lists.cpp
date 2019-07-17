720881d88ded3874d6c7ac1094be342b799f82f5
ling.shi <ling.shi@horizon.ai>
2019-07-17 14:11:20 +0800

    完成第21题

--- 21.merge-two-sorted-lists.cpp
+++ 21.merge-two-sorted-lists.cpp
@@ -14,7 +14,36 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
+        ListNode* HeadPtr = NULL;
+        ListNode* OperatorPtr;
+        ListNode* O1Ptr = l1;
+        ListNode* O2Ptr = l2;
+        ListNode* NewPtr = NULL;
+        while(O1Ptr != NULL || O2Ptr != NULL) {
+            if(O1Ptr == NULL && O2Ptr != NULL) {
+                NewPtr = O2Ptr;
+                O2Ptr = O2Ptr->next;
+            } else if(O1Ptr != NULL && O2Ptr == NULL) {
+                NewPtr = O1Ptr;
+                O1Ptr = O1Ptr->next;
+            } else {
+                if(O1Ptr->val < O2Ptr->val) {
+                    NewPtr = O1Ptr;
+                    O1Ptr = O1Ptr->next;
+                } else {
+                    NewPtr = O2Ptr;
+                    O2Ptr = O2Ptr->next;
+               }
+            }
+            if(HeadPtr == NULL) {
+                HeadPtr = NewPtr;
+                OperatorPtr = HeadPtr;
+            } else {
+                OperatorPtr->next = NewPtr;
+                OperatorPtr = OperatorPtr->next;
+            }
+        }
+        return HeadPtr;
     }
 };
 
